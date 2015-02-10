c63d64b55ddc480702091a570aa8a4e49eff2045
Ryan Grasell <rgrasell@gmail.com>
2015-02-09 20:16:01 -0800

    More efficient google API interface

--- webserver/googleInterface.py
+++ webserver/googleInterface.py
@@ -4,14 +4,17 @@ URL = 'https://www.googleapis.com/oauth2/v2/userinfo'
 
 class ApiInterface(object):
 	token = None
+	JSON = None
+
 	def __init__(self, token):
 		self.token = token
+		self.JSON = requests.get(URL, headers={'Authorization': 'Bearer ' + self.token}).json()
 
 	def getEmail(self):
-		return self.getJSON()['email']
+		return self.JSON['email']
 
 	def getId(self):
-		return self.getJSON()['id']
+		return self.JSON['id']
 
 	def getJSON(self):
-		return requests.get(URL, headers={'Authorization': 'Bearer ' + self.token}).json()
+		return self.JSON
